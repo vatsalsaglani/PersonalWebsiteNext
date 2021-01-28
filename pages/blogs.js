403766362710ee3b65a80061897a25189b744cbf
vatsals <vatsals@quinnox.com>
2021-01-28 22:35:04 +0530

    added to pages, changed color to neumorphism

---
+++ pages/blogs.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {
+  ChakraProvider,
+  Container,
+  Link,
+  Text,
+  Input,
+  Button,
+  useMediaQuery,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  Drawer,
+  IconButton,
+} from '@chakra-ui/react'
+
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+import { useState } from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Blogs</h1>
+        </div>
+    )
+}
